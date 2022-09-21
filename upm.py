4c8feaf692c44c3da9ba2eda030f8131d32782c7
Notmemayeb <notmemaybeatwork@gmail.com>
2022-09-21 23:14:56 +0200

    Comments for upm.py

--- upm.py
+++ upm.py
@@ -12,13 +12,16 @@ def collect_data(url):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    # Starts chrome in headless mode
     driver.get(url)
     return driver.page_source
+    # Gets data form url
 
 
 def parse_data(data):
     soup = BeautifulSoup(data, 'lxml')
     content = soup.find('div', id='contenido')
+    # Gets blocks of content
     ps = content.findAll('p', {'style': ' text-align: center;'})
     blocks = []
     for p in ps:
@@ -26,23 +29,27 @@ def parse_data(data):
         block.append(p.find('strong').text)
         block.append(p.findNext('div'))
         blocks.append(block)
+        # Creates array of blocks
 
     grades = []
 
     for item in blocks:
         tds = item[1].findAll('td')
+        # Finds every grade in block
         for td in tds:
             grade = {}
             grade['dir'] = item[0]
             grade['name'] = td.find('strong').text.replace('\n', '')
             grade['href'] = td.find('strong').findParent().get('href')
             grade['center'] = td.text.split('\n')[1]
+            # Gets data form grades
             grades.append(grade)
+            # Creates array of grades
 
     return grades
 
 
-if __name__ == "__upm__":
+if __name__ == "__main__":
     data = collect_data(url1)
     grades = parse_data(data)
     json_grades = json.dumps(grades)
