89092386ae90e7be5a196a66fdb196d4f82688ad
notmemayeb <notmemaybeatwork@gmail.com>
2022-10-14 09:43:05 +0200

    Some cosmetical changes in main.py

--- main.py
+++ main.py
@@ -12,16 +12,32 @@ url_upm = "https://www.upm.es/Estudiantes/Estudios_Titulaciones/EstudiosOficiale
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-driver.get(url_upm)
-upm_data = driver.page_source
+# Starts chrome driver in headless mode
 
-
+def get_data(url, univers_name):
+    print(f'Getting data from {univers_name}...')
+    driver.get(url)
+    # Driver goes to *url*
+    print('Done!')
+    return driver.page_source
 
 def create_json(univers_name, data, parse_data_func):
+    print(f'Parsing {univers_name} data...')
     grades = parse_data_func(data)
+    print(f'Creating {univers_name}.json...')
     json_grades = json.dumps(grades)
     with open(f'{univers_name}.json', 'w', encoding="utf-8") as file:
         file.write(json_grades)
+    print(f'Done! Look for {univers_name}.json')
+    # Dumps data to json and saves it in $university_name.json
+
+
+
+def main():
+    create_json('upm', get_data(url_upm, 'upm'), parse_upm)
 
 if __name__ == "__main__":
-    create_json('upm', upm_data, parse_upm)
+    try:
+        main()
+    except Exception as exc:
+        print(exc)
